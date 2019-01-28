d96915223c6387da87f78805d3aa8ed551bcc142
Karol Majewski <karol-majewski@users.noreply.github.com>
2019-01-28 22:39:54 +0100

    Get rid of 'dist' in require statement path

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// TODO: get rid of 'dist' in require statement path
 // TODO: get rid of '.Claim;' at the end of the require statement
-const Claim = require('my-lib-54357435/dist/security/Claim').Claim;
+const Claim = require('my-lib-54357435').Claim;
 const claim = new Claim('tenantId', '123');
 
 /** @type {AuthenticatedRequest} */ // TODO: be able to use my-lib.http.AuthenticatedRequest
