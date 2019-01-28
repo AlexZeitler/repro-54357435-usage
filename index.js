2a16bcfa67862bd668f018bfc55970a8ad35c993
Karol Majewski <karol-majewski@users.noreply.github.com>
2019-01-28 22:42:00 +0100

    Import your AuthenticatedRequest type

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const { Claim } = require('my-lib-54357435');
 const claim = new Claim('tenantId', '123');
 
-/** @type {AuthenticatedRequest} */ // TODO: be able to use my-lib.http.AuthenticatedRequest
+/** @type {import('my-lib-54357435').AuthenticatedRequest} */
 const request = {};
