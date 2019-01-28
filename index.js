cc2a63729dd536dcdd86747c5eaa40b71c37e39d
Karol Majewski <karol-majewski@users.noreply.github.com>
2019-01-28 22:40:16 +0100

    Get rid of '.Claim;' at the end of the require statement

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// TODO: get rid of '.Claim;' at the end of the require statement
-const Claim = require('my-lib-54357435').Claim;
+const { Claim } = require('my-lib-54357435');
 const claim = new Claim('tenantId', '123');
 
 /** @type {AuthenticatedRequest} */ // TODO: be able to use my-lib.http.AuthenticatedRequest
